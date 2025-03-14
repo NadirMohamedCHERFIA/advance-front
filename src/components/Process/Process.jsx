82e3bf9459b65350f4608313fce4f304a7d1c31f
CHERFIA Mohamed Nadir <mohamednadir.cherfia@gmail.com>
2025-03-14 09:33:14 +0100

    Added nodes

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -11,11 +11,18 @@ import React, { useCallback } from 'react';
 import '@xyflow/react/dist/style.css';
  
 const initialNodes = [
-  { id: '1', position: { x: 100, y: 100 }, data: { label: 'Fichiers Excel' } },
+  { id: '1', position: { x: 100, y: 100 }, data: { label: 'Getting Data' } },
   { id: '2', position: { x: 200, y: 200 }, data: { label: 'Formatting' } },
-  { id: '3', position: { x: 300, y: 300 }, data: { label: 'ADVance Algo' } },
+  { id: '3', position: { x: 300, y: 300 }, data: { label: 'Creating connections' } },
+  { id: '4', position: { x: 500, y: 350 }, data: { label: 'Checking Warranties' } },
+  { id: '5', position: { x: 500, y: 450 }, data: { label: 'Checking if user/New' } },
 ];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2',animated: true },{ id: 'e2-3', source: '2', target: '3',animated: true }];
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2',animated: true },
+  { id: 'e2-3', source: '2', target: '3',animated: true },
+  { id: 'e3-4', source: '3', target: '4',animated: true },
+  { id: 'e3-5', source: '3', target: '5',animated: true }
+];
 
 const Process = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
@@ -26,8 +33,11 @@ const Process = () => {
     [setEdges],
   );
   return (
-    <div className="">
-      <div style={{ width: '100vw', height: '100vh', marginTop:'120px'}}>
+    <div className="" style={{marginTop:"120px"}}>
+      <div className="fs-3 text-light text-center fw-bold w-100">
+        ADVance process
+      </div>
+      <div style={{ width: '100vw', height: '100vh'}}>
         <ReactFlow
           nodes={nodes}
           edges={edges}
