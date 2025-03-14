642abf7ba230313fa517574dfa99b68c56eb39dc
CHERFIA Mohamed Nadir <mohamednadir.cherfia@gmail.com>
2025-03-14 10:20:39 +0100

    Added nodes axemple

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -9,19 +9,30 @@ import React, { useCallback } from 'react';
   addEdge,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
- 
+import { FcProcess } from "react-icons/fc";
+
+import { AiOutlineDatabase } from "react-icons/ai";
+
+
 const initialNodes = [
-  { id: '1', position: { x: 100, y: 100 }, data: { label: 'Getting Data' } },
-  { id: '2', position: { x: 200, y: 200 }, data: { label: 'Formatting' } },
-  { id: '3', position: { x: 300, y: 300 }, data: { label: 'Creating connections' } },
-  { id: '4', position: { x: 500, y: 350 }, data: { label: 'Checking Warranties' } },
-  { id: '5', position: { x: 500, y: 450 }, data: { label: 'Checking if user/New' } },
+  { id: '1', position: { x: 100, y: 100 }, data: { label: 'Getting Data' } , style:{backgroundColor:"#1bb495",color:"white"}},
+  { id: '2', position: { x: 200, y: 150 }, data: { label: 'Formatting' } , style:{backgroundColor:"#1bb495",color:"white"}},
+  { id: '3', position: { x: 300, y: 200 }, data: { label: 'Creating connections'} , style:{backgroundColor:"#1bb495",color:"white"}},
+  { id: '4', position: { x: 500, y: 350 }, data: { label: 'Listing Shop services'} , style:{backgroundColor:"#b41b93",color:"white"}},
+  { id: '5', position: { x: 500, y: 450 }, data: { label: 'Listing used parts'} , style:{backgroundColor:"#b41b93",color:"white"}},
+  { id: '6', position: { x: 500, y: 550 }, data: { label: 'Listing SO'} , style:{backgroundColor:"#b41b93",color:"white"}},
+  { id: '7', position: { x: 700, y: 450 }, data: { label: 'Check if NEW/USED'} , style:{backgroundColor:"#1b8eb4",color:"white"}},
+  { id: '8', position: { x: 900, y: 600 }, data: { label: 'Creating a summary'} , style:{backgroundColor:"#9cba17",color:"white"}},
 ];
+
 const initialEdges = [
   { id: 'e1-2', source: '1', target: '2',animated: true },
   { id: 'e2-3', source: '2', target: '3',animated: true },
   { id: 'e3-4', source: '3', target: '4',animated: true },
-  { id: 'e3-5', source: '3', target: '5',animated: true }
+  { id: 'e3-5', source: '3', target: '5',animated: true },
+  { id: 'e3-6', source: '3', target: '6',animated: true },
+  { id: 'e5-7', source: '5', target: '7',animated: true },
+  { id: 'e7-8', source: '7', target: '8',animated: true },
 ];
 
 const Process = () => {
@@ -33,17 +44,19 @@ const Process = () => {
     [setEdges],
   );
   return (
-    <div className="" style={{marginTop:"120px"}}>
-      <div className="fs-3 text-light text-center fw-bold w-100">
-        ADVance process
+    <div className="" style={{marginTop:"110px"}}>
+      <div className="fs-1 text-light text-center fw-bold w-100 mb-1 d-flex justify-content-center align-items-center gap-2 pl-5">
+        <FcProcess/> 
+        <h3 className='fs-1 fw-bold'>Process</h3>
       </div>
-      <div style={{ width: '100vw', height: '100vh'}}>
+      <div style={{ width: '100vw', height: '70vh'}}>
         <ReactFlow
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
+          style={{backgroundColor:""}}
         >
           <Controls />
           <MiniMap />
@@ -56,34 +69,3 @@ const Process = () => {
 
 export default Process
 
-
-// import React, { useCallback } from 'react';
-
- 
-// import '@xyflow/react/dist/style.css';
- 
-// const initialNodes = [
-//   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-//   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-// ];
-// const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
-// export default function App() {
-
- 
-//   return (
-//     <div style={{ width: '100vw', height: '100vh' }}>
-//       <ReactFlow
-//         nodes={nodes}
-//         edges={edges}
-//         onNodesChange={onNodesChange}
-//         onEdgesChange={onEdgesChange}
-//         onConnect={onConnect}
-//       >
-//         <Controls />
-//         <MiniMap />
-//         <Background variant="dots" gap={12} size={1} />
-//       </ReactFlow>
-//     </div>
-//   );
-// }
--- src/components/copyrights/Copyrights.jsx
+++ src/components/copyrights/Copyrights.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Cockies = () => {
   return (
-    <div className='container text-secondary text-center' style={{marginTop:"100px"}}>
+    <div className='container text-secondary text-center' style={{marginTop:"50px"}}>
         <p>© 2025 SAE LA</p>
     </div>
   )
